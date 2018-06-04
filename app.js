ea9385837508e8c8d3912d527264a7caf33482e7
Kief H. Shemul <theshemul@gmail.com>
2018-06-04 12:22:10 +0600

    added timestamp in model and init mongodb

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var createError = require('http-errors');
 var express = require('express');
+var mongoose = require("mongoose");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var trupleRouter = require('./routes/trupleRoutes');
+
+// Database init 
+mongoose.Promise = global.Promise;
+if (process.env.env == "development") {
+  mongoose.connect("mongodb://127.0.0.1:4001/interview_db")
+}
+
+
 var app = express();
 app.set('view engine', 'jade');
 
@@ -15,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/values' , trupleRouter);
+app.use('/values', trupleRouter);
 
 
 // catch 404 and forward to error handler
